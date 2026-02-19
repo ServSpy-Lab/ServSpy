08a633d5dbfcbec91136e41cbe91cb6696ae02a8
RayXu <xu_ruihong2009@163.com>
2026-02-19 20:58:48 +0800

    core-code: add the file transfer function in tcp connection file and optimize the performance (#19)

--- test/test_TCP.py
+++ test/test_TCP.py
@@ -8,14 +8,27 @@ if package_dictionary not in os.sys.path:
 class TestConnect:
     def __init__(self):
         create_tcp_client_amount=int(input())
-        self.TCP_server_process=subprocess.Popen(
-            ["python", os.path.join(os.path.dirname(__file__), 'test_TCP_server.py')], 
-            creationflags=subprocess.CREATE_NEW_CONSOLE)
-        time.sleep(2)
-        for i in range(create_tcp_client_amount):
-            time.sleep(0.5)
-            subprocess.Popen(
-                ["python", os.path.join(os.path.dirname(__file__), 'test_TCP_client.py')], 
+        if sys.platform.startswith('win'):
+            self.TCP_server_process=subprocess.Popen(
+                ["python", os.path.join(os.path.dirname(__file__), 'test_TCP_server.py')], 
                 creationflags=subprocess.CREATE_NEW_CONSOLE)
+            time.sleep(2)
+            for i in range(create_tcp_client_amount):
+                time.sleep(0.5)
+                subprocess.Popen(
+                    ["python", os.path.join(os.path.dirname(__file__), 'test_TCP_client.py')], 
+                    creationflags=subprocess.CREATE_NEW_CONSOLE)
+        else:
+            cmd = [
+                'gnome-terminal', '--', 'bash', '-c', "python3.14 {}".format( 
+                os.path.join(os.path.dirname(__file__), 'test_TCP_server.py'))]
+            self.TCP_server_process=subprocess.Popen(cmd)
+            time.sleep(2)
+            for i in range(create_tcp_client_amount):
+                cmd=[
+                    'gnome-terminal', '--', 'bash', '-c', "python3.14 {}".format(
+                    os.path.join(os.path.dirname(__file__), 'test_TCP_client.py'))]
+                time.sleep(0.5)
+                subprocess.Popen(cmd)
 if __name__=="__main__":
     TestConnect()
