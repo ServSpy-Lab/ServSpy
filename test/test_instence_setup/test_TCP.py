ff5bbad922a91193961a854f8ea29b8b26047ef3
F18-Ray <xu_ruihong2009@126.com>
2026-05-26 18:21:44 +0800

    tests: move the instence tests to another dictionary

--- test/test_instence_setup/test_TCP.py
+++ test/test_instence_setup/test_TCP.py
@@ -2,7 +2,7 @@ import os
 import sys
 import time
 import subprocess
-package_dictionary=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+package_dictionary=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if package_dictionary not in os.sys.path:
     sys.path.insert(0, package_dictionary)
 class TestConnect:
--- test/test_instence_setup/test_TCP_server.py
+++ test/test_instence_setup/test_TCP_server.py
@@ -1,6 +1,6 @@
 import os
 import sys
-package_dictionary=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+package_dictionary=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 if package_dictionary not in os.sys.path:
     sys.path.insert(0, package_dictionary)
 from src import connect_tcp
