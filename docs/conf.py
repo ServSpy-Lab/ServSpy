76eece570af8212c6421f9b3a7aee82f1ca27dd4
F18-Ray <xu_ruihong2009@126.com>
2026-06-17 18:58:39 +0800

    Docs: updating conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ extensions = []
 
 locale_dirs = ['locale/']
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'locale/**/*.po', 'locale/**/*.bak']
 language='en'
 
 
